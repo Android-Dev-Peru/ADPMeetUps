b2f37438987580566fa817505846868433eae04b
Jose Flavio <jflavio90@gmail.com>
2024-08-10 10:01:55 -0500

    refactor main codebase

--- composeApp/src/commonMain/kotlin/domain/usecase/GetEventInfo.kt
+++ composeApp/src/commonMain/kotlin/domain/usecase/GetEventInfo.kt
@@ -1,13 +1,11 @@
 package domain.usecase
 
-import data.datasource.Failure
-import data.datasource.ResultType
 import data.repository.EventsRepository
 import domain.Event
 import kotlinx.datetime.LocalDate
 
 class GetEventInfo(private val eventsRepository: EventsRepository) {
-    suspend fun invoke(date: LocalDate): ResultType<Event, Failure> {
-        return ResultType.Success(eventsRepository.getEventInfo(date))
+    suspend fun invoke(date: LocalDate): Result<Event> {
+        return eventsRepository.getEventInfo(date)
     }
 }
