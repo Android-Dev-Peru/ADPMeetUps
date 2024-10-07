6dfdc2ee0c013d14df848a238b2ea2e5e22e7c98
Jose Flavio <jflavio90@gmail.com>
2024-10-06 20:12:45 -0500

    add compose State collectors for multiplatform

---
+++ composeApp/src/iosMain/kotlin/core/ComposeStateCollectors.ios.kt
@@ -0,0 +1,12 @@
+package core
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.State
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import kotlinx.coroutines.flow.StateFlow
+import kotlin.coroutines.CoroutineContext
+
+@Composable
+actual fun <T> StateFlow<T>.collectAsStateMultiplatform(context: CoroutineContext): State<T> {
+    return collectAsStateWithLifecycle()
+}
